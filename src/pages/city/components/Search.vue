3e9a280458ce565a0947faf34ef7b8bec7702f57
hungryfoolou <1039622586@qq.com>
2020-06-25 10:45:05 +0800

    finish search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,69 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  <div>
+    <div class="search">
+      <!-- v-model：双向绑定 -->
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音"/>
+    </div>
+    <!-- keyword为空时就不隐藏 -->
+    <div class="search-content" ref="searchref" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <!-- 如果搜索结果为空，则显示"没有找到匹配数据"-->
+        <li class="search-item border-bottom" v-show="hasEmptyResult">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '', // 关键字
+      list: [], // 存放关键字搜索结果
+      timer: null
+    }
+  },
+  computed: {
+    hasEmptyResult () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      // 用节流函数提高效率
+      if (this.timer) {
+        // 如果已经存在了，即通过执行if方框下面的代码给timer赋值
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) { // 比如用户搜索结束后又清空了搜索框，那么搜索结果列表也应该清空
+        this.list = []
+        return // 不必执行下面代码
+      }
+      /* 如果你正在做这个括号里面的事情，那么延迟16毫秒再执行这个事情,
+      如果在这16毫秒之间你又做了本次函数操作,则清除上次操作,转而执行这次要做的事情 */
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) { // 遍历每个字母
+          this.cities[i].forEach((value) => { // 遍历每个字母下的所有城市键值对
+            // 搜索到了关键词
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result // 把搜索结果放到this.list中
+      }, 100) // 间隔100毫秒
+    }
+  },
+  mounted () { // 加载完就执行betterscroll操作
+    this.scroll = new BScroll(this.$refs.searchref)
+  }
 }
 </script>
 
@@ -25,4 +82,18 @@ export default {
       text-align: center
       border-radius: .06rem
       color: #666 // 输入字体的颜色
+  .search-content
+    z-index: 1 // 参考：https://www.w3school.com.cn/cssref/pr_pos_z-index.asp
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    left: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      color: #666
+      background: #fff
 </style>
